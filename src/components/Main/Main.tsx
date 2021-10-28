9538e5666a7ea7bad6f5c774884ab2ddac2c3ca6
uretashi <alexandrehoffmann50@gmail.com>
2021-10-28 22:45:58 +0200

    Main stucture added

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Main.module.css';
+
+
+const Main = () => (
+  <div className={styles.Main}>
+    Main Component
+  </div>
+);
+
+export default Main;
