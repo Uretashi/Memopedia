814a4ba876002aadd9d61c30591f6db0bc6ecbb9
uretashi <alexandrehoffmann50@gmail.com>
2021-11-02 22:03:54 +0100

    conflict resolved

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 2a10106db610478ceae01783629af00f4919ea3f
 
 ReactDOM.render(
   <React.StrictMode>
