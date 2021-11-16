368a33e0b50a17a8c03a09d3f8c2f466fd0d1644
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2021-11-16 21:56:54 +0100

    First modifications on main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Main.module.css';
+import { Button, Container, Row, Col, ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import {
     BrowserRouter,
     Routes,
@@ -14,17 +15,63 @@ import PostMeme from "../PostMeme/PostMeme";
 
 const Main = () => (
 
-<main className={`${styles.Main} text-white`}>
-    <h1>Hello UwU</h1>
-    <BrowserRouter>
-        <Routes>
-            <Route path="search/:tags" element={ <Memesearch /> } />
-            <Route path="postMeme" element={ <PostMeme /> } />
-            <Route path="login" element={ <Login /> } />
-            <Route path="createAccount" element={ <CreateAccount /> } />
-        </Routes>
-    </BrowserRouter>
-  </main>
+    <main>
+        <Container fluid>
+            <Row>
+                <Col>
+                    <div className={`${styles.DivButtons}`}>
+                        <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+                            <ToggleButton className={`${styles.boutonDeMerde}`} id="tbg-radio-1" value={1}>
+                                Last meme
+                            </ToggleButton>
+                            <ToggleButton className={`${styles.boutonDeMerde}`} id="tbg-radio-2" value={2}>
+                                Last user
+                            </ToggleButton>
+                            <ToggleButton className={`${styles.boutonDeMerde}`} id="tbg-radio-3" value={3}>
+                                Last tag
+                            </ToggleButton>
+                        </ToggleButtonGroup>
+                    </div>
+                </Col>
+                <Col>
+                    
+                </Col>
+            </Row>
+        </Container>
+
+        <Container className={`${styles.NoNameYet}`}>
+            <Row className={`${styles.NoNameYet}`}>
+                <Col xs={5}>
+                    <div className={`${styles.divLastUsers}`}>
+                        -qlq
+                        -qlqd'autre
+                        -jugepas
+                    </div>
+                </Col>
+                <Col xs={5}>
+                    <div className={`${styles.FewMemes}`}>
+
+                    </div>
+                </Col>
+                <Col>
+                    <div className={`${styles.FewMemes}`}>
+
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+
+
+        <h1 className={`${styles.Main} text-white`}>Hello UwU</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="search/:tags" element={<Memesearch />} />
+                <Route path="postMeme" element={<PostMeme />} />
+                <Route path="login" element={<Login />} />
+                <Route path="createAccount" element={<CreateAccount />} />
+            </Routes>
+        </BrowserRouter>
+    </main>
 );
 
 export default Main;
