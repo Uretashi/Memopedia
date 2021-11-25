5fc9d32ce94b5dd3dc288506f52c1a56b2cfcf50
uretashi <alexandrehoffmann50@gmail.com>
2021-11-25 20:43:40 +0100

    api request init

--- src/components/Meme/Meme.tsx
+++ src/components/Meme/Meme.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import styles from './Meme.module.css';
 
-const Meme = () => (
-  <div className={styles.Meme}>
-    Meme Component
-  </div>
-);
+
+class Meme extends React.Component {
+
+    // constructor(props: any) {
+    //     super(props);
+    // }
+
+    memes = [];
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className={styles.Meme}>
+                Meme Component
+            </div>
+        );
+    }
+}
+
+// const Meme = () => (
+//   <div className={styles.Meme}>
+//     Meme Component
+//   </div>
+// );
 
 export default Meme;
--- src/components/Memesearch/Memesearch.tsx
+++ src/components/Memesearch/Memesearch.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import styles from './Memesearch.module.css';
 
-const Memesearch = () => (
-  <div className={styles.Memesearch}>
-    Memesearch Component
-  </div>
-);
+require('dotenv').config();
 
-export default Memesearch;
+export default class Memesearch extends React.Component {
+
+    memes = [];
+
+    componentDidMount() {
+        fetch(`${process.env.REACT_APP_API_URL}/memes/byTag?tags=youtube,gif`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result)
+                },
+                (error) => {
+                    console.log('errrrr mdrrrrr salope')
+                });
+
+    }
+
+    render() {
+        return (
+            <div className={styles.Memesearch}>
+                Memesearch Component
+            </div>
+        );
+    }
+}
+
+// export default Memesearch;
