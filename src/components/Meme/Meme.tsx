5e173ecd8ac793a4884207b98a02e414eba6d27f
Vincent Rabajoie <rabajoie.vincent@gmail.com>
2021-11-26 22:02:33 +0100

    Ajouts sur la page Meme et fix du footer

--- src/components/Meme/Meme.tsx
+++ src/components/Meme/Meme.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './Meme.module.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
+import meme1 from '../../assets/meme/meme6.jpg';
+
 require('dotenv').config();
 
 export default class Meme extends React.Component {
@@ -26,20 +28,21 @@ export default class Meme extends React.Component {
                 <div>
                     <h1>ParamTitle</h1>
                     <div className={styles.DivContainer}>
-                        <div className={styles.DivImage}>
-                            <img className={styles.MemeImage} />
-                        </div>
-
-                        <Container>
+                        <Row className="justify-content-md-center mb-3">
+                            <Col className={styles.DivImage} xs={12} sm={4} md={8}>
+                                <img src={meme1} className={styles.MemeImage} />
+                            </Col>
+                        </Row>
+                        <Container className={styles.Contain}>
                             <Row>
                                 <Col xs={8}>
                                     <div className={styles.DivDescription}>
-                                        test
+                                        <p className={styles.WriteSection}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                                     </div>
                                 </Col>
                                 <Col>
                                     <div className={styles.DivVote}>
-                                        Test
+                                        <p className={styles.WriteSection}>Likes: param</p>
                                     </div>
                                 </Col>
                             </Row>
